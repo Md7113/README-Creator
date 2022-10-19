ddc4dc617f990537fc8ea17a4785a5757c3e1f72
Mason <davismasont@gmail.com>
2022-10-19 00:04:12 -0700

    finished project functions

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require('fs')
+const user = require("./utils/generateMarkdown")
+
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
-
 //title
 //Description
 //Table of contents
@@ -25,54 +17,77 @@ init();
 //gitHub in questions
 //email in questions
 //TOC links must work
+const questions = [{
+    name: 'title',
+    message: 'What Is the title of your README?',
+    type: 'input'
+   },{
+    name:'Description',
+    message: 'write a comprehensive description of your Repository:',
+    type: 'input'
+   },{
+    name: 'TableOfContents',
+    message: 'select every option you want in your table of contents, use Space bar to select an option <a> to select all and <i> to invert your selections:',
+    type: 'checkbox',
+    choices: ['1. [Title](#Title)','2. [description](#Description)','3. [Table of Contents](#Table)','4. [Instalation](#Instalation)','5. [Usage](#Usage)','6. [License](#License)','7. [Contributing](#Contributing)','8. [Test](#Tests)','9. [Questions](#Questions)']
+   },{
+    name:'Instalation',
+    message: 'Write Instalation instructions if aplicable, if not Write N/A and this option will not be included: ',
+    type: 'input'
+   },{
+    name:'Usage',
+    message: 'Write a description about the usage of your portfolio or write N/A if you dont want it included:',
+    type: 'input'
+   },{
+    name: 'License',
+    message: 'Which license do you want to use for your README:',
+    type: 'list',
+    choices: ['none','Apaches License 2.0','GNU General Public License v3.0','MIT License','BSD 2-Clause "Simplified" License','BSD 3-Clasue "New" or "revised" License','Boost Software License 1.0','Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0','GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0','The Unlicense']
+   },{
+    name:'Contributing',
+    message: 'List of all contributers seperated by a comma, Ex[jon, doe, jim, Harper] or N/A if this does not apply:',
+    type: 'input'
+   },{
+    name:'tests',
+    message: 'describe any tests you have:',
+    type: 'input'
+   },{
+    name:'email',
+    message: 'enter your email so people can reach you:',
+    type: 'input'
+   },{
+    name:'github',
+    message: 'enter your github so people can see your other creations:',
+    type: 'input'
+   }];
 
-inquirer.prompt([
-      {
-        name: 'title',
-        message: 'What Is the title of your README?',
-        type: 'input'
-       },{
-        name:'Description',
-        message: 'write a comprehensive description of your Repository:',
-        type: 'input'
-       },{
-        name: 'TableOfContents',
-        message: 'select every option you want in your table of contents, use Space bar to select an option <a> to select all and <i> to invert your selections:',
-        type: 'checkbox',
-        choices: ['title','description','Table of Contents','Instalation','Usage','License','Contributing','Test','Questions']
-       },{
-        name:'Instalation',
-        message: 'Write Instalation instructions if aplicable, if not Write N/A and this option will not be included: ',
-        type: 'input'
-       },{
-        name:'Usage',
-        message: 'Write a description about the usage of your portfolio or write N/A if you dont want it included:',
-        type: 'input'
-       },{
-        name: 'License',
-        message: 'Which license do you want to use for your README:',
-        type: 'list',
-        choices: ['none','Apaches License 2.0','GNU General Public License v3.0','MIT License','BSD 2-Clause "Simplified" License','BSD 3-Clasue "New" or "revised" License','Boost Software License 1.0','Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0','GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0','The Unlicense']
-       },{
-        name:'Contributing',
-        message: 'List of all contributers seperated by a comma, Ex[jon, doe, jim, Harper] or N/A if this does not apply:',
-        type: 'input'
-       },{
-        name:'tests',
-        message: 'describe any tests you have:',
-        type: 'input'
-       },{
-        name:'email',
-        message: 'enter your email so people can reach you:',
-        type: 'input'
-       },{
-        name:'github',
-        message: 'enter your github so people can see your other creations:',
-        type: 'input'
-       }
-    ])
-      .then(function(answer){
-       console.log(answer);
-       console.log(answer.TableOfContents)
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data ,function(err){
+        if(err){
+            console.log(err)
+        }
+    })
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+      .then(function(userAnswer){
        console.log(answer.Contributing.split(','))
-      });
+       console.log(`${user(userAnswer)}`)
+       let file = userAnswer.title + '-README.md'
+       writeToFile(file,user(userAnswer))
+
+      })
+    }
+
+// Function call to initialize app
+init();
+
+
+
+
+      
+      
+      
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,91 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let badgeArr = ['','[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+  '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)',
+  '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+  '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+  '[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)',
+  '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+  '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+  '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+  '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
+  '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)']
+
+  let licenseArr = ['none','Apaches License 2.0','GNU General Public License v3.0','MIT License','BSD 2-Clause "Simplified" License',
+  'BSD 3-Clasue "New" or "revised" License','Boost Software License 1.0','Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0', 
+  'GNU Affero General Public License v3.0', 'GNU General Public License v2.0','GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0',
+  'The Unlicense']
+
+  let badgeAns = licenseArr.indexOf(data.License)
+  let badgeShow = badgeArr[badgeAns]
+
+  return `#Title
+
+${data.title}
+
+
+
+
+##Description
+
+${data.Description}
+
+
+
+
+##Table Of Contents
+
+${data.TableOfContents.join('\n')}
+
+
+
+
+##Instalation
+
+${data.Instalation}
+
+
+
+
+
+##Usage
+
+${data.Usage}
+
+
+
+
+##License
+
+${badgeShow}
+
+${data.License}
+
+
+
+
+
+##Contributing
+
+${data.Contributing.split(',').join('\n')}
+
+
+
+
+##Tests
+
+${data.tests}
+
+
+
+
+##Questions
+
+If you have any questions you can reach me at ${data.email} , or check out my GitHub at ${data.github}
 
 `;
 }
